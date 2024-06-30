bb24e8f4df8d90e13a7954d9b626df5d4353a5be
MMZidan <mostafa.mzidan@outlook.com>
2024-07-01 01:57:47 +0300

    Update pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+pattern=int(input("Enter the size of the pattern: "))
+itr = 0
+while pattern > itr :
+    for i in range(pattern):
+        print("*", end="")
+    itr = itr+1
+    print("\n")
+    
+        
