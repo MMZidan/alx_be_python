f8e7e6b910a1f649054f4739a0ad5ba192cf7216
MMZidan <mostafa.mzidan@outlook.com>
2024-07-31 01:20:45 +0300

    shopping shopping_list_manager upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,39 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        print(choice,type(choice))
+        if choice == '1':
+            # Prompt for and add an item
+            add = input("Add Item:  ")
+            shopping_list.append(add)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            print(shopping_list)
+            remove=input("Remove Item:  ")
+            try:
+                shopping_list.remove(remove)
+            except:
+                 print("An exception occurred") 
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
