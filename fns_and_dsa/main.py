c55e5df4d8a3751d0b6d45ee30148b282985a4f9
MMZidan <mostafa.mzidan@outlook.com>
2024-07-31 00:39:45 +0300

    update 3

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -5,11 +5,8 @@ def main():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    if operation=='divide' and num2==0 :
-        print("Can't divide by Zero")
-    else: 
-        result = perform_operation(num1, num2, operation)
-        print(f"Result: {result}")
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
 
 if __name__ == "__main__":
     main()
