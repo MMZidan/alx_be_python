cb856d0f0923613297062599fa20f133d2bf6e8a
MMZidan <mostafa.mzidan@outlook.com>
2024-08-17 05:00:37 +0300

    OOP Project 1 V1.2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,24 +1,26 @@
 
 class BankAccount:
     def __init__(self,balance):
-        # self.balance = 0.0
+        #  self.balance = 0.0
         self.balance=balance
 
     def deposit(self, deposit_amount):
         self.balance += deposit_amount
 
     def withdraw(self, withdraw_amount):
-        self.balance -= withdraw_amount
-        # if self.balance >= withdraw_amount:
-        #     self.balance -= withdraw_amount
-        #     return True
-        # else:
-        #     print("Insufficient funds.")
-        #     return False
+        # self.balance -= withdraw_amount
+        if self.balance >= withdraw_amount:
+            self.balance -= withdraw_amount
+            return True
+        else:
+            # print("Insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: $ {self.balance}")
-# Account=BankAccount()
+# Account=BankAccount(100)
+# Account.withdraw(20)
+# Account.display_balance()
 # # withdraw_amount=float(input("Please Enter withdraw amount: "))
 # deposit_amount=float(input("Please Enter deposit amount: "))
 # Account.deposit(deposit_amount)
